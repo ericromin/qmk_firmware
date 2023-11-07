ff1d541cd8fdaa7f01d44fddab621b4df12dd6fc
Eric Romin <eric.romin@gmail.com>
2023-11-06 22:45:31 -0600

    Add math operators to _NUM layer

--- keyboards/crkbd/rev1/keymaps/ericromin/keymap.c
+++ keyboards/crkbd/rev1/keymaps/ericromin/keymap.c
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX,TG(_NUM),TG(_NAV), XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       XXXXXXX,TG(_NUM),TG(_NAV), XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
                          _______, _______, _______,    _______, _______, _______
   //                   ╰───────────────────────────╯ ╰───────────────────────────╯
@@ -113,11 +113,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT_36(
   // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-      TO(_WIN),TO(_MAC), XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX,
+      TO(_WIN),TO(_MAC), XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    KC_7,    KC_8,    KC_9, KC_MINS,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX,
+       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,    KC_4,    KC_5,    KC_6,  KC_EQL,
   // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX,TG(_NUM),TG(_NAV), XXXXXXX, XXXXXXX,    XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,
+       XXXXXXX,TG(_NUM),TG(_NAV), XXXXXXX, XXXXXXX,    XXXXXXX,    KC_1,    KC_2,    KC_3, _______,
   // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
                          _______, _______, _______,    _______, _______,    KC_0
   //                   ╰───────────────────────────╯ ╰───────────────────────────╯
